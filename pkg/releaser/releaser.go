391906747fd4de0b18d7524cc664da510f1bfc3a
Aidan Jensen <aidan@artificial.com>
2023-03-22 20:08:02 -0700

    local repo only

--- pkg/releaser/releaser.go
+++ pkg/releaser/releaser.go
@@ -11,7 +11,7 @@ import (
 	"github.com/rajatjindal/krew-release-bot/pkg/source/actions"
 )
 
-//Releaser is what opens PR
+// Releaser is what opens PR
 type Releaser struct {
 	Token                         string
 	TokenEmail                    string
@@ -29,12 +29,12 @@ func getCloneURL(owner, repo string) string {
 	return fmt.Sprintf("https://github.com/%s/%s.git", owner, repo)
 }
 
-//TODO: get email, userhandle, name from token
+// TODO: get email, userhandle, name from token
 func getUserDetails(token string) (string, string, string) {
 	return "krew-release-bot", "Krew Release Bot", "krewpluginreleasebot@gmail.com"
 }
 
-//New returns new releaser object
+// New returns new releaser object
 func New(ghToken string) *Releaser {
 	tokenUserHandle, tokenUsername, tokenEmail := getUserDetails(ghToken)
 
@@ -48,11 +48,11 @@ func New(ghToken string) *Releaser {
 		UpstreamKrewIndexRepoCloneURL: getCloneURL(krew.GetKrewIndexRepoOwner(), krew.GetKrewIndexRepoName()),
 		LocalKrewIndexRepo:            krew.GetKrewIndexRepoName(),
 		LocalKrewIndexRepoOwner:       tokenUserHandle,
-		LocalKrewIndexRepoCloneURL:    "https://github.com/krew-release-bot/krew-index.git",
+		LocalKrewIndexRepoCloneURL:    "https://github.com/artificialinc/artificial-krew-repository.git",
 	}
 }
 
-//HandleActionLambdaWebhook handles requests from github actions
+// HandleActionLambdaWebhook handles requests from github actions
 func (releaser *Releaser) HandleActionLambdaWebhook(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	hook, err := actions.NewGithubActions()
 	if err != nil {
@@ -84,7 +84,7 @@ func (releaser *Releaser) HandleActionLambdaWebhook(ctx context.Context, request
 	}, nil
 }
 
-//HandleActionWebhook handles requests from github actions
+// HandleActionWebhook handles requests from github actions
 func (releaser *Releaser) HandleActionWebhook(w http.ResponseWriter, r *http.Request) {
 	hook, err := actions.NewGithubActions()
 	if err != nil {
