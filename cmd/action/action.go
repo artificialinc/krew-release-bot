21e1debfe262f6fb1e1496070977e3dd68f323e2
Aidan Jensen <aidan@artificial.com>
2023-03-23 07:05:28 -0700

    debug env var

--- cmd/action/action.go
+++ cmd/action/action.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/rajatjindal/krew-release-bot/pkg/source/actions"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -15,6 +17,9 @@ var actionCmd = &cobra.Command{
 	Use:   "action",
 	Short: "github action for updating plugin manifests in krew-index repo",
 	Run: func(cmd *cobra.Command, args []string) {
+		if os.Getenv("LOG_LEVEL") == "debug" {
+			logrus.SetLevel(logrus.DebugLevel)
+		}
 		err := actions.RunAction()
 		if err != nil {
 			logrus.Fatal(err)
